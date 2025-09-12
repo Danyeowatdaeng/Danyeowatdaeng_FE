ae1fe12f076d921465af8c4bc4bb185fa1c6d25f
Jimin Nam <dnpsel2737@gmail.com>
2025-09-13 00:42:28 +0900

    feat: BackHeader 수정

--- src/components/molecules/BackHeader.tsx
+++ src/components/molecules/BackHeader.tsx
@@ -7,11 +7,11 @@ type BackHeaderProps = { onBack?: () => void; label?: string };
 
 export default function BackHeader({ onBack, label }: BackHeaderProps) {
   return (
-    <header className="w-full h-[7vh] flex items-start gap-5">
+    <header className="w-full flex items-center gap-5">
       <Button aria-label="뒤로가기" onClick={onBack}>
         <ChevronLeft aria-label="chevron-left" aria-hidden />
       </Button>
-      <Label content={label ?? ""}/>
+      <Label content={label ?? ""} className="text-[20px] font-bold"/>
     </header>
   );
 }
