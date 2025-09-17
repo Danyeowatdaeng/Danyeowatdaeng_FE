65792399a14b248b518201745e0e4d6970637cd6
민수민 <soo@minsumin-ui-MacBookPro.local>
2025-09-17 23:11:53 +0900

    chore: test를 위해 로그인 리다이렉트 로직 일시 삭제

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,20 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import HomeLandingPage from "../pages/HomeLandingPage";
 
 export const Route = createFileRoute("/")({
-  beforeLoad: ({ location }) => {
-    if (location.pathname === "/") {
-      // access_token이 쿠키에 없으면 /login으로 리다이렉트
-      if (typeof document !== "undefined") {
-        const hasToken = document.cookie
-          .split(";")
-          .some((cookie) => cookie.trim().startsWith("access_token="));
-        if (!hasToken) {
-          throw redirect({ to: "/login" });
-        }
-      }
-    }
-  },
+  //   beforeLoad: ({ location }) => {
+  //     if (location.pathname === "/") {
+  //       // access_token이 쿠키에 없으면 /login으로 리다이렉트
+  //       if (typeof document !== "undefined") {
+  //         const hasToken = document.cookie
+  //           .split(";")
+  //           .some((cookie) => cookie.trim().startsWith("access_token="));
+  //         if (!hasToken) {
+  //           throw redirect({ to: "/login" });
+  //         }
+  //       }
+  //     }
+  //   },
   component: RouteComponent,
 });
 
