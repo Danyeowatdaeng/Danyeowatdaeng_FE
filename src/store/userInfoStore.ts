3fbf85a0e25253987f699da704618701a9d4a05f
Yoonhojoon <dbsghwns1209@gmail.com>
2025-09-21 21:32:37 +0900

    [HOTFIX]: OAuth2 인증 후 리다이렉트 처리 및 사용자 정보 저장 기능 추가

--- src/store/userInfoStore.ts
+++ src/store/userInfoStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface UserInfo {
   isLogin: boolean;
@@ -9,13 +10,25 @@ interface UserInfo {
   setPetImage: (image: null | File) => void;
 }
 
-const useUserInfoStore = create<UserInfo>((set) => ({
-  isLogin: false,
-  setIsLogin: (value) => set({ isLogin: value }),
-  petAvatarId: 1,
-  setPetAvatarId: (id: number) => set({ petAvatarId: id }),
-  petImage: null,
-  setPetImage: (image: null | File) => set({ petImage: image }),
-}));
+const useUserInfoStore = create<UserInfo>()(
+  persist(
+    (set) => ({
+      isLogin: false,
+      setIsLogin: (value) => set({ isLogin: value }),
+      petAvatarId: 1,
+      setPetAvatarId: (id: number) => set({ petAvatarId: id }),
+      petImage: null,
+      setPetImage: (image: null | File) => set({ petImage: image }),
+    }),
+    {
+      name: "user-info-storage",
+      // petImage는 File 객체이므로 localStorage에 저장하지 않음
+      partialize: (state) => ({
+        isLogin: state.isLogin,
+        petAvatarId: state.petAvatarId,
+      }),
+    }
+  )
+);
 
 export default useUserInfoStore;
