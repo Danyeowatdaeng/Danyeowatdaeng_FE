bca72505026cd5a6e2673e822d0ce9636ed076d6
Yoonhojoon <dbsghwns1209@gmail.com>
2025-10-27 04:53:40 +0900

    feat: integrate user info store in DailyQuestTemplate for dynamic pet avatar rendering

--- src/components/templates/DailyQuestTemplate.tsx
+++ src/components/templates/DailyQuestTemplate.tsx
@@ -1,6 +1,7 @@
 import BackHeader from "../molecules/BackHeader";
 import DailyProgress from "../organisms/mypet/DailyProgress";
 import QuestList from "../organisms/mypet/QuestList";
+import useUserInfoStore from "../../store/userInfoStore";
 
 type Props = {
   onBack?: () => void;
@@ -21,15 +22,21 @@ export default function DailyQuestTemplate({
   onDiary,
   onReview,
 }: Props) {
+  const { memberInfo } = useUserInfoStore();
+
   return (
     <div className="p-6 my-5">
       {/* 헤더 */}
       <BackHeader onBack={onBack} label="일일퀘스트" />
 
-      {/* 펫 캐릭터 (임시) */}
+      {/* 펫 캐릭터 */}
       <div className="w-full flex justify-center">
         <img
-          src="/Assets/icons/PetAvatar1.svg"
+          src={
+            memberInfo?.petAvatarCdnUrl 
+              ? `https://${memberInfo.petAvatarCdnUrl}` 
+              : memberInfo?.profileImageUrl || "/Assets/icons/PetAvatar1.svg"
+          }
           alt=""
           aria-hidden
           className="w-[180px] h-[214px] object-contain"
